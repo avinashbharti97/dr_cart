b1b1b528d3eb41c3b21efcfbdeff18ec3c21dbd4
avinashbharti97 <avinashbharti97@gmail.com>
2018-03-10 22:52:25 +0530

    flatpages added and a flatpage about created

--- dr_cart/dr_cart/urls.py
+++ dr_cart/dr_cart/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from main import views as main_views
 admin.autodiscover()
@@ -21,5 +21,6 @@ admin.autodiscover()
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', main_views.index, name='home'),
+    url(r'^pages/', include('django.contrib.flatpages.urls'))
 
 ]
